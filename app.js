1468b0d8ccce3123a460aa9128b4270906866026
Andres9092 <richardsonandres1@gmail.com>
2022-10-17 19:53:03 -0300

    avance ejs 17/10 tarde

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const mainRoutes = require('./src/routes/main.js');
 
 app.use('/', mainRoutes);
 
-app.set('views', path.join(__dirname,'src/views'))
+app.set('views', path.join(__dirname,'src/views')) //necesario ya que archivos que estan en carpeta VIEWS, no estan en carpeta raiz, si no que dentro de SRC.
 
-app.set('view engine','ejs')
+app.set('view engine','ejs')  //seteo el motor
 
